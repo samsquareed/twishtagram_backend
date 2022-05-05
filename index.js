58f31e706795e6b4271c804140238a5c5dd2cba5
samsquareed <samsquare.professional@gmail.com>
2022-05-05 14:03:59 +0530

    reorganized index.js

--- index.js
+++ index.js
@@ -3,32 +3,30 @@ import mongoose from "mongoose";
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-dotenv.config()
 import AuthRoute from "./routes/auth.js"
 import PostRoute from "./routes/posts.js"
 
+dotenv.config()
 const app = express();
-app.use(cors());
+
 // app.use(express.json())
-const PORT = process.env.PORT || 8080;
 
 //middlewares
 app.use(bodyParser.json({ limit: '20mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }))
-
-
-mongoose.connect(process.env.MONGOURI, ()=>{
-    console.log("connected to Db");
-})
-
-
+app.use(cors());
 //custom routes middlewares
 app.use("/auth", AuthRoute)
 app.use("/post",PostRoute)
 
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res)=>{
     res.send("Twistagram developed by samsquare")
+})
+
+mongoose.connect(process.env.MONGOURI, ()=>{
+    console.log("connected to Db");
 })
 
 app.listen(PORT, ()=>{
